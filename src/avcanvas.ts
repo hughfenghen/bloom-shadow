49e07e1ec7ffbefbaf8392fb5843edaa7b1da88c
hughfenghen <hughfenghen@gmail.com>
2023-09-04 22:57:11 +0800

    feat: 编码视频帧

--- src/avcanvas.ts
+++ src/avcanvas.ts
@@ -35,17 +35,89 @@ function start() {
     const vfReadable = new MediaStreamTrackProcessor({
       track: videoTrack
     }).readable
-    autoReadStream(vfReadable, {
-      onChunk: async (vf) => {
-        console.log(55555, vf.duration, vf.timestamp)
-        vf.close()
-      },
-      onDone: () => {
 
+    let fps = 0
+    const t = performance.now()
+    autoReadStream(vfReadable.pipeThrough(new class extends TransformStream<VideoFrame, EncodedVideoChunk> {
+      constructor() {
+        let ctrl: TransformStreamDefaultController | null = null
+        const encoder = new VideoEncoder({
+          error: console.error,
+          output: (chunk) => {
+            ctrl?.enqueue(chunk)
+          }
+        })
+        encoder.configure({
+          codec: 'avc1.4D0032',
+          framerate: 30,
+          // hardwareAcceleration: 'prefer-hardware',
+          // 码率
+          bitrate: 2e4,
+          width: 1280,
+          height: 720,
+          // H264 不支持背景透明度
+          alpha: 'discard',
+          // macos 自带播放器只支持avc
+          avc: { format: 'avc' }
+          // mp4box.js 无法解析 annexb 的 mimeCodec ，只会显示 avc1
+          // avc: { format: 'annexb' }
+        })
+
+        const transVF = encodeVideoFrame(30)
+        super({
+          start: (c) => {
+            ctrl = c
+          },
+          transform(vf) {
+            const data = transVF(vf)
+            if (data == null) return
+            encoder.encode(data.vf, data.opts)
+            data.vf.close()
+          }
+        })
       }
+    }), {
+      onChunk: async (chunk) => {
+        console.log(666, ~~(fps / (performance.now() - t) * 1000), chunk)
+        fps += 1
+      },
+      onDone: () => { }
     })
   }
 }
+
+function encodeVideoFrame(expectFPS: number) {
+  const startTime = performance.now()
+  let lastTime = startTime
+
+  const maxFPS = expectFPS * 1.1
+
+  let frameCnt = 0
+  return (frame: VideoFrame) => {
+    const now = performance.now()
+    const offsetTime = now - startTime
+    // 避免帧率超出期望太高
+    if ((frameCnt / offsetTime) * 1000 > maxFPS) {
+      frame.close()
+      return
+    }
+
+    const vf = new VideoFrame(frame, {
+      // timestamp 单位 微妙
+      timestamp: offsetTime * 1000,
+      duration: (now - lastTime) * 1000
+    })
+    lastTime = now
+
+    frameCnt += 1
+    frame.close()
+    return {
+      vf,
+      opts: { keyFrame: frameCnt % 150 === 0 }
+    }
+  }
+}
+
 
 function autoReadStream<ST extends ReadableStream>(
   stream: ST,
