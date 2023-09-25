ae24faefb28a7152370fc40fd061af51388f8a43
hughfenghen <hughfenghen@gmail.com>
2023-09-25 22:51:36 +0800

    fix:  预览元素重复多个

--- src/ui/export-files.tsx
+++ src/ui/export-files.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useAtom } from "jotai";
-import { IExpFile, expFilesAtom } from "../store/store";
+import React, { useEffect, useState } from 'react';
+import { useAtom } from 'jotai';
+import { IExpFile, expFilesAtom } from '../store/store';
 
 export function ExpFiles() {
   const [expFiles, setExpFiles] = useAtom(expFilesAtom);
   const [pvFile, setPVFile] = useState<null | IExpFile>(null);
 
   const genFile = (f: IExpFile, isPreview = false) => {
-    return f.type === "video" ? (
+    return f.type === 'video' ? (
       <video src={f.url} controls={isPreview}></video>
     ) : (
       <div></div>
@@ -15,13 +15,13 @@ export function ExpFiles() {
   };
 
   useEffect(() => {
-    const onKeyDown = ({ key }: React.KeyboardEvent<HTMLDivElement>) => {
-      if (key === "Escape" && pvFile != null) setPVFile(null);
+    const onKeyDown = ({ key }: KeyboardEvent) => {
+      if (key === 'Escape' && pvFile != null) setPVFile(null);
     };
-    document.addEventListener("keydown", onKeyDown, false);
+    document.addEventListener('keydown', onKeyDown);
 
     return () => {
-      document.removeEventListener("keydown", onKeyDown, false);
+      document.removeEventListener('keydown', onKeyDown);
     };
   }, [pvFile]);
 
@@ -48,21 +48,21 @@ export function ExpFiles() {
             {f.type} {f.duration}s
           </div>
           <div className="text-center">{f.createTime}</div>
-          {pvFile && (
-            <div
-              onClick={() => setPVFile(null)}
-              className="fixed z-50 top-0 left-0 w-full h-full bg-[#0009]"
-            >
-              <div
-                onClick={(e) => e.stopPropagation()}
-                className="absolute w-[900px] h-[500px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-              >
-                {genFile(f, true)}
-              </div>
-            </div>
-          )}
         </div>
       ))}
+      {pvFile && (
+        <div
+          onClick={() => setPVFile(null)}
+          className="fixed z-50 top-0 left-0 w-full h-full bg-[#0009]"
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="absolute w-[900px] h-[500px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          >
+            {genFile(pvFile, true)}
+          </div>
+        </div>
+      )}
     </>
   );
 }
@@ -74,7 +74,7 @@ const FileMask: React.FC<
     onDownload: () => void;
   }>
 > = function FileMask({ children, onDelete, onPreview, onDownload }) {
-  const btnCls = "m-auto border rounded-full inline-block h-[40px] w-[40px]";
+  const btnCls = 'm-auto border rounded-full inline-block h-[40px] w-[40px]';
   return (
     <div className="relative text-white group text-xs">
       <div className="absolute w-full h-full bg-[#00000066] hidden group-hover:flex items-center content-center z-10 px-8">
@@ -94,11 +94,11 @@ const FileMask: React.FC<
 };
 
 function download(url: string, createTime: string) {
-  const aEl = document.createElement("a");
+  const aEl = document.createElement('a');
   document.body.appendChild(aEl);
-  aEl.setAttribute("href", url);
-  aEl.setAttribute("download", `bloom-shadow-${createTime}.mp4`);
-  aEl.setAttribute("target", "_self");
+  aEl.setAttribute('href', url);
+  aEl.setAttribute('download', `bloom-shadow-${createTime}.mp4`);
+  aEl.setAttribute('target', '_self');
   aEl.click();
   aEl.remove();
 }
