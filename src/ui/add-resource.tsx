936733b31f565287e4c5d59e5ee7664958bdaebc
hughfenghen <hughfenghen@gmail.com>
2023-09-29 18:34:47 +0800

    feat: 分享屏幕、演讲者 位置

--- src/ui/add-resource.tsx
+++ src/ui/add-resource.tsx
@@ -18,12 +18,48 @@ export function AddResource() {
               audioCtx: avCvs.spriteManager.audioCtx,
             });
             await avCvs.spriteManager.addSprite(vs);
+            // 自适应全屏
+            const { w, h } = vs.rect;
+            const ratio = w / h;
+            if (ratio > 1920 / 1080) {
+              vs.rect.x = 0;
+              vs.rect.w = 1920;
+              vs.rect.h = vs.rect.w / ratio;
+              vs.rect.y = (1080 - vs.rect.h) / 2;
+            } else {
+              vs.rect.y = 0;
+              vs.rect.h = 1080;
+              vs.rect.w = vs.rect.h * ratio;
+              vs.rect.x = (1920 - vs.rect.w) / 2;
+            }
           }}
         >
           分享屏幕
         </button>
-        <button className="border border-solid border-black px-2">
-          摄像头/麦克风
+        <button
+          className="border border-solid border-black px-2"
+          onClick={async () => {
+            const mediaStream = await navigator.mediaDevices.getUserMedia({
+              video: {
+                width: 900,
+                height: 600,
+              },
+              audio: true,
+            });
+            const avCvs = recorder.getAVCanvas();
+            const vs = new VideoSprite('speaker', mediaStream, {
+              audioCtx: avCvs.spriteManager.audioCtx,
+            });
+            await avCvs.spriteManager.addSprite(vs);
+            // 默认右下角
+            const margin = 1920 * 0.015;
+            vs.rect.w = 450;
+            vs.rect.h = 300;
+            vs.rect.x = 1920 - vs.rect.w - margin;
+            vs.rect.y = 1080 - vs.rect.h - margin;
+          }}
+        >
+          演讲者
         </button>
       </div>
     </div>
